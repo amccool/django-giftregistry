04625ff8bf4bb232f739e0ea3cc5350053a92a33
amccool <alex.mccool@yahoo.com>
2011-08-17 01:17:31 -0700

    touch

---
+++ build/pygraphviz/pygraphviz/version.py
@@ -0,0 +1,11 @@
+"""
+Version information for PyGraphviz, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__version__ = '1.1.dev1947'
+__revision__ = '1947'
+__date__ = 'Tue Sep 21 22:10:08 2010'
+
